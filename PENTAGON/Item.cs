c90acab3632310294c73264cde4ebd863dbe080b
정채운 <toyboy01113@gmail.com>
2023-11-15 21:02:06 +0900

    초기 클래스 구조 형성

---
+++ PENTAGON/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PENTAGON
+{
+    public class Item
+    {
+    }
+
+    public class EquipItem : Item
+    {
+
+    }
+
+    public class WeaponItem : EquipItem
+    {
+
+    }
+
+    public class ArmorItem : EquipItem
+    {
+
+    }
+
+    public class PotionItem : Item
+    {
+
+    }
+}
--- PENTAGON/Program.cs
+++ PENTAGON/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            //코드 컨벤션 
+            //요구사항 정리
+            //
             Console.WriteLine("Hello, World!");
         }
     }
