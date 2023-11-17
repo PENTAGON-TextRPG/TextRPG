08ca1c8a8cb4ec4917804098d77e5ad327787cff
min-vely <jeongmin322@naver.com>
2023-11-17 14:10:25 +0900

    Mp가 0으로 나오는 현상 수정

--- PENTAGON/Player.cs
+++ PENTAGON/Player.cs
@@ -9,7 +9,7 @@ using System.Xml.Linq;
 using EnumsNamespace;
 using static System.Net.Mime.MediaTypeNames;
 
-namespace PENTAGON
+namespace ConsoleApp1
 {
     public abstract class Player : Character
     {
@@ -23,13 +23,52 @@ namespace PENTAGON
         public int _fSkillDamage;
         public float _sSkillDamage;
 
+        private int _level = 1;
+        private int _mp;
+        private int _maxMp;
+        private Inventory _inventory = new Inventory();
+        private JobType _job;
+        private Item[] _equipmentWeaponArray = new Item[2];
+        //private Item[] _equipmentArmorArray = new Item[5];
+
         Monster monster;
         List<Monster> monsters;
+
+        public int Level
+        {
+            get { return _level; }
+            set { _level = value; }
+        }
+        public int Mp
+        {
+            get { return _mp; }
+            set { _mp = value; }
+        }
+        public int MaxMp
+        {
+            get { return _maxMp; }
+        }
+        public Inventory Inventory
+        {
+            get { return _inventory; }
+            set { _inventory = value; }
+        }
+        public JobType JobType
+        {
+            get { return _job; }
+            set { _job = value; }
+        }
+        public Item[] EquipmentWeaponArray
+        {
+            get { return _equipmentWeaponArray; }
+            set { }
+        }
 
 
         public Player(string name)
         {
             _name = name;
+            _mp = 30;
 
             // 생성자에서 초기화
             InitializeMonsters();
@@ -44,23 +83,60 @@ namespace PENTAGON
         public abstract void DisplayMyInfo();
 
         //inventory 생성
-        //인벤토리
+        // 인벤토리
         public void DisplayInventory()
         {
+            Console.Clear();
+            Console.WriteLine("인벤토리");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
+            Console.WriteLine("[아이템 목록]");
+            // 아이템 리스트
+            Console.WriteLine();
+            Console.WriteLine("1. 장착 관리");
+            Console.WriteLine("0. 나가기\n");
+            Console.WriteLine("원하시는 행동을 입력해주세요.\n");
 
+            int input = Program.CheckValidInput(0, 1);
+            switch (input)
+            {
+                case 0:
+                    Program.DisplayGameIntro();
+                    break;
+                case 1:
+                    DisplayEquipManage();
+                    break;
+            }
         }
 
         //장착 관리
         public void DisplayEquipManage()
         {
+            Console.Clear();
+            Console.WriteLine("인벤토리 - 장착 관리");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
+            Console.WriteLine("[아이템 목록]");
+            // 아이템 리스트
+            Console.WriteLine("1. 아이템 장착 or 사용하기");
+            Console.WriteLine("0. 나가기\n");
+            Console.WriteLine("원하시는 행동을 입력해주세요.\n");
 
+            int input = Program.CheckValidInput(0, 1);
+            switch (input)
+            {
+                case 0:
+                    DisplayInventory();
+                    break;
+                case 1:
+                    // 아이템 장착
+                    break;
+            }
         }
 
         //스킬 사용
         public void UseSkill()
         {
             Console.Clear();
-            Console.WriteLine($"내 현재 MP : {Program.player1._mp}");
+            Console.WriteLine($"내 현재 MP : {Program.player1.Mp}");
             Console.WriteLine($"1. {_fSkillName} - MP {_fSkillMp}");
             Console.WriteLine($"{_fSkillInfo}\n");
             Console.WriteLine($"2. {_sSkillName} - MP {_sSkillMp}");
@@ -68,14 +144,24 @@ namespace PENTAGON
             Console.WriteLine("사용할 스킬을 고르세요.");
 
             int input = Program.CheckValidInput(1, 2);
-            switch (input)
+            // 플레이어의 MP가 선택한 스킬의 소모 MP보다 적은지 확인
+            if ((input == 1 && Program.player1.Mp < _fSkillMp) || (input == 2 && Program.player1.Mp < _sSkillMp))
             {
-                case 1:
-                    FirstSkill();
-                    break;
-                case 2:
-                    SecondSkill();
-                    break;
+                Console.WriteLine("MP가 부족하여 스킬을 사용할 수 없습니다.");
+                Thread.Sleep(3000);
+                Program.DisplayGameIntro();
+            }
+            else
+            {
+                switch (input)
+                {
+                    case 1:
+                        FirstSkill();
+                        break;
+                    case 2:
+                        SecondSkill();
+                        break;
+                }
             }
         }
 
@@ -95,21 +181,23 @@ namespace PENTAGON
             Console.Clear();
             Console.WriteLine($"{_name}이(가) {selectedMonster}에게 {_fSkillName}을(를) 사용하여 {damage}의 데미지를 입혔습니다.\n");
 
-            //몬스터를 죽여 경험치 획득
+            // 몬스터를 죽여 경험치 획득
             if (selectedMonster.IsDie())
             {
                 int monsterExp = selectedMonster.Exp;
                 Console.WriteLine($"{selectedMonster}을(를) 죽였습니다! 획득한 경험치: {monsterExp}\n");
-                Console.WriteLine($"남은 MP : {Program.player1.Mp}와 {_sSkillMp}의 차\n");
-                Console.WriteLine("");
-                Program.player1._mp -= _fSkillMp; //Program.player1._mp가 0으로 출력되는 문제
+
+                Console.WriteLine($"남은 MP : {Program.player1.Mp - _fSkillMp}\n");
+                Program.player1.Mp -= _fSkillMp; //Program.player1._mp가 0으로 출력되는 문제 해결
                 GainExp(monsterExp);
             }
-            else
+            else // 몬스터가 죽지 않으면 경험치 미획득
             {
                 Console.WriteLine($"하지만 {selectedMonster}는 살아남았네요 . . .\n");
+                Console.WriteLine($"남은 MP : {Program.player1.Mp - _fSkillMp}\n");
+                Program.player1.Mp -= _fSkillMp;
             }
-            Thread.Sleep(4000);
+            Thread.Sleep(5000);
             Program.DisplayGameIntro();
             //UseSkill();
         }
@@ -144,8 +232,6 @@ namespace PENTAGON
             {
                 int monsterExp1 = selectedMonster1.Exp;
                 Console.WriteLine($"{selectedMonster1}을(를) 죽였습니다! 획득한 경험치: {monsterExp1}\n");
-                Console.WriteLine($"남은 MP : {Program.player1.Mp}와 {_sSkillMp}의 차\n");
-                Program.player1._mp -= _sSkillMp;
                 GainExp(monsterExp1);
             }
             else
@@ -157,8 +243,6 @@ namespace PENTAGON
             {
                 int monsterExp2 = selectedMonster2.Exp;
                 Console.WriteLine($"{selectedMonster2}을(를) 죽였습니다! 획득한 경험치: {monsterExp2}\n");
-                Console.WriteLine($"남은 MP : {Program.player1.Mp}와 {_sSkillMp}의 차\n");
-                Program.player1._mp -= _sSkillMp;
                 GainExp(monsterExp2);
             }
             else
@@ -166,6 +250,8 @@ namespace PENTAGON
                 Console.WriteLine($"하지만 {selectedMonster2}는 살아남았네요 . . .\n");
             }
 
+            Console.WriteLine($"남은 MP : {Program.player1.Mp - _sSkillMp}\n");
+            Program.player1.Mp -= _sSkillMp;
             Thread.Sleep(5000);
             UseSkill();
         }
@@ -188,8 +274,8 @@ namespace PENTAGON
         {
             Level++;
             Exp = 0; // 레벨업 후 경험치 초기화
-            //_attack++; // 기본 공격력 1 증가
-            //_defence++; // 기본 방어력 1 증가
+                     //_attack++; // 기본 공격력 1 증가 오류뜸
+                     //_defence++; // 기본 방어력 1 증가 오류뜸
 
             Console.WriteLine($"{_name}이(가) Lv.{Level}로 레벨업했습니다!");
         }
@@ -219,46 +305,6 @@ namespace PENTAGON
                 return 0;
             }
         }
-
-
-        public int Level
-        {
-            get { return _level; }
-            set { _level = value; }
-        }
-        public int Mp
-        {
-            get { return _mp; }
-            set { _mp = value; }
-        }
-        public int MaxMp
-        {
-            get { return _maxMp; }
-        }
-        public Inventory Inventory
-        {
-            get { return _inventory; }
-            set { _inventory = value; }
-        }
-        public JobType JobType
-        {
-            get { return _job; }
-            set { _job = value; }
-        }
-        public Item[] EquipmentWeaponArray
-        {
-            get { return _equipmentWeaponArray; }
-            set { }
-        }
-
-        private int _level = 1;
-        private int _mp;
-        private int _maxMp;
-        private Inventory _inventory = new Inventory();
-        private JobType _job;
-        private Item[] _equipmentWeaponArray = new Item[2];
-        private Item[] _equipmentArmorArray = new Item[5];
-
     }
 
 
@@ -273,12 +319,15 @@ namespace PENTAGON
             // Warrior의 스킬 설정
             _fSkillName = "전사 스킬 1";
             _sSkillName = "전사 스킬 2";
-            _fSkillMp = 15;
-            _sSkillMp = 25;
+            _fSkillMp = 10;
+            _sSkillMp = 20;
             _fSkillInfo = "전사의 스킬 1입니다.";
             _sSkillInfo = "전사의 스킬 2입니다.";
             _fSkillDamage = _attack * 2;
             _sSkillDamage = _attack * 1.5f;
+            Mp = 30;
+            _attack = 15;
+            _defence = 15;
         }
 
         public override void DisplayMyInfo()
@@ -295,7 +344,7 @@ namespace PENTAGON
             int addDefence = _defence - _initialDefence;
             Console.WriteLine($"방어력: {_defence}" + (addDefence != 0 ? $" (+{addDefence})" : ""));
             Console.WriteLine($"체력: {_hp}");
-            Console.WriteLine($"MP: {_mp}");
+            Console.WriteLine($"MP: {Program.player1.Mp}");
             Console.WriteLine($"Gold : {Gold} G");
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
@@ -314,23 +363,16 @@ namespace PENTAGON
         // 데미지 받는 메서드
         public override void ReceiveDamage(int damage, DamageType damageType)
         {
-            if (damageType == DamageType.DT_Skill)
+
+            if (damage <= _defence) damage = 1;
+            else damage -= _defence;
+
+            Hp -= damage;
+
+            if (Hp < 0)
             {
-                if (damage <= Defence) damage = 1;
-                else damage -= Defence;
-
-                Hp -= damage;
-
-                if (Hp < 0)
-                {
-                    Hp = 0;
-                }
+                Hp = 0;
             }
-            else if (damageType == DamageType.DT_Normal)
-            {
-                //10퍼 확률로 회피
-            }
-
         }
 
         // 공격하는 메서드
@@ -361,8 +403,8 @@ namespace PENTAGON
         private const int _initialDefence = 15;
         public int _hp = 40;
         private int _maxHp = 40;
-        public int _mp = 20;
-        private int _maxMp = 20;
+        public int _mp = 30;
+        private int _maxMp = 30;
         public int _attack = 15;
         private int _defence = 15;
 
@@ -420,8 +462,8 @@ namespace PENTAGON
     //    private const int _initialDefence = 5;
     //    private int _hp = 30;
     //    private int _maxHp = 30;
-    //    private int _mp = 20;
-    //    private int _maxMp = 20;
+    //    private int _mp = 30;
+    //    private int _maxMp = 30;
     //    private int _attack = 20;
     //    private int _defence = 5;
 
--- PENTAGON/Program.cs
+++ PENTAGON/Program.cs
@@ -5,120 +5,118 @@ namespace PENTAGON
 {
     internal class Program
     {
-            public static Player player1;
-            public static void DisplayGameIntro()
+        public static void DisplayGameIntro()
+        {
+            Console.Clear();
+
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("1. 상태보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("3. 상점");
+            Console.WriteLine("4. 던전 입장");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.Write(">>");
+
+            int input = CheckValidInput(1, 4);
+            switch (input)
             {
-                Console.Clear();
-
-                Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
-                Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
-                Console.WriteLine();
-                Console.WriteLine("1. 상태보기");
-                Console.WriteLine("2. 인벤토리");
-                Console.WriteLine("3. 상점");
-                Console.WriteLine("4. 던전 입장");
-                Console.WriteLine();
-                Console.WriteLine("원하시는 행동을 입력해주세요.");
-                Console.Write(">>");
-
-                int input = CheckValidInput(1, 4);
-                switch (input)
-                {
-                    case 1:
+                case 1:
                     Console.WriteLine("상태를 보여주는 메서드를 넣어주세요.");
-                        break;
+                    break;
 
-                    case 2:
+                case 2:
                     Console.WriteLine("인벤토리를 보여주는 메서드를 넣어주세요.");
                     break;
-                    case 3:
+                case 3:
                     Console.WriteLine("상점을 보여주는 메서드를 넣어주세요.");
                     break;
-                    case 4:
+                case 4:
                     Console.WriteLine("던전을 보여주는 메서드를 넣어주세요.");
                     break;
             }
-            }
+        }
 
-            public static int CheckValidInput(int min, int max)
-            {
-                while (true)
-                {
-                    string input = Console.ReadLine();
-
-                    bool parseSuccess = int.TryParse(input, out var ret);
-                    if (parseSuccess)
-                    {
-                        if (ret >= min && ret <= max)
-                            return ret;
-                    }
-
-                    Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요");
-                    Console.Write(">>");
-                }
-            }
-
-        public static Player ChoiceJob(string nickname)
+        public static int CheckValidInput(int min, int max)
         {
-            Console.WriteLine("직업을 선택해주세요");
-            Console.WriteLine("1. 전사");
-            Console.WriteLine("2. 마법사");
-            Console.WriteLine("3. 도적");
-            Console.WriteLine("4. 궁수");
-            Console.WriteLine();
-            Console.Write(">>");
+            while (true)
+            {
+                string input = Console.ReadLine();
 
-            Player p1;
-            int input = CheckValidInput(1, 4);
+                bool parseSuccess = int.TryParse(input, out var ret);
+                if (parseSuccess)
+                {
+                    if (ret >= min && ret <= max)
+                        return ret;
+                }
 
-            if (input % 4 == 1)
-            {
-                p1 = new Warrior(nickname);
-                return p1;
-            }
-            else if (input % 4 == 2)
-            {
-                p1 = new Mage(nickname);
-                return p1;
-            }
-            else if (input == 3)
-            {
-                p1 = new Thief(nickname);
-                return p1;
-            }
-            else
-            {
-                p1 = new Archer(nickname);
-                return p1;
+                Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요");
+                Console.Write(">>");
             }
         }
 
+        //public static Player ChoiceJob(string nickname)
+        //{
+        //    Console.WriteLine("직업을 선택해주세요");
+        //    Console.WriteLine("1. 전사");
+        //    Console.WriteLine("2. 마법사");
+        //    Console.WriteLine("3. 도적");
+        //    Console.WriteLine("4. 궁수");
+        //    Console.WriteLine();
+        //    Console.Write(">>");
+
+        //    int input = CheckValidInput(1, 4);
+
+        //    //if (input % 4 == 1)
+        //    //{
+        //    //    p1 = new Warrior(nickname);
+        //    //    return p1;
+        //    //}
+        //    //else if (input % 4 == 2)
+        //    //{
+        //    //    p1 = new Mage(nickname);
+        //    //    return p1;
+        //    //}
+        //    //else if (input == 3)
+        //    //{
+        //    //    p1 = new Thief(nickname);
+        //    //    return p1;
+        //    //}
+        //    //else
+        //    //{
+        //    //    p1 = new Archer(nickname);
+        //    //    return p1;
+        //    //}
+        //}
+
 
         public static string SetNickname()
+        {
+            string nickname;
+            do
             {
-                string nickname;
-                do
+                Console.WriteLine("닉네임을 입력하세요:");
+                nickname = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(nickname))
                 {
-                    Console.WriteLine("닉네임을 입력하세요:");
-                    nickname = Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("닉네임이 올바르지 않습니다. 다시 시도하세요.");
+                }
 
-                    if (string.IsNullOrWhiteSpace(nickname))
-                    {
-                        Console.Clear();
-                        Console.WriteLine("닉네임이 올바르지 않습니다. 다시 시도하세요.");
-                    }
+            } while (string.IsNullOrWhiteSpace(nickname));
 
-                } while (string.IsNullOrWhiteSpace(nickname));
+            return nickname;
+        }
 
-                return nickname;
-            }
+        static void Main()
+        {
+            string nickname = SetNickname();
+            //플레이어 직업 선택
+            DisplayGameIntro();
+        }
 
-            static void Main()
-            {
-                string nickname = SetNickname();
-                player1 = ChoiceJob(nickname);
-                DisplayGameIntro();
-            }
- 
     }
 }
