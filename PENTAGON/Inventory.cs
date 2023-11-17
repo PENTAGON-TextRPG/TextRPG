5bb7f46f40e9b01d5463aa93eeb50a0f9bd7803a
Neunggwon <104968524+Neunggwon@users.noreply.github.com>
2023-11-17 14:53:44 +0900

    Update
    UPdate

--- PENTAGON/Inventory.cs
+++ PENTAGON/Inventory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleTables;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,173 @@ namespace PENTAGON
 {
     public class Inventory
     {
+        List<Item> itemList = new List<Item>();
+        List<Item> inventory = new List<Item>();
+        List<Item> weaponItem = new List<Item>();
+        List<Item> armorItem = new List<Item>();
+        //List<Item> potionItem = new List<Item>();
+        List<PotionItem> potionItem = new List<PotionItem>();
+
+        public void ItemSetting()
+        {
+            Item ironArmor = new Item("무쇠 갑옷", 0, 0, 5, "흔히 볼 수 있는 갑옷입니다.", Job.Null, false);
+            armorItem.Add(ironArmor);
+
+            Item oldSword = new Item("낡은 검", 0, 5, 0, "흔히 볼 수 있는 검입니다.", Job.Null, false);
+            weaponItem.Add(oldSword);
+
+            PotionItem potion = new PotionItem("물약", 50, "물약을 먹으면 HP가 회복됩니다.");
+            potionItem.Add(potion);
+        }
+
+        public void InventoryMain()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("인벤토리");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("1. 무기 인벤토리");
+            Console.WriteLine("2. 방어구 인벤토리");
+            Console.WriteLine("3. 포션 인벤토리");
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.Write(">>");
+        }
+        //인벤토리 화면..
+        public void DisplayInventory()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("인벤토리");
+            Console.ResetColor();
+            var table = new ConsoleTable("이름", "능력치", "설명");
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                //if (inventory[i].Equip == true)
+                if (inventory[i].Name.Contains("[E]"))
+                {
+                    table.AddRow($"{inventory[i].Name} ", $"공격력:{inventory[i].Atk} 방어력:{inventory[i].Atk}", $"{inventory[i].Explanation}");
+                }
+                else
+                {
+                    table.AddRow($"[E] {inventory[i].Name} ", $"공격력:{inventory[i].Atk} 방어력:{inventory[i].Atk}", $"{inventory[i].Explanation}");
+                }
+            }
+            table.Write();
+            //Console.WriteLine();
+            //Console.WriteLine("1. 무기 인벤토리");
+            //Console.WriteLine("2. 방어구 인벤토리");
+            //Console.WriteLine("3. 포션 인벤토리");
+            //Console.WriteLine("0. 나가기");
+
+            int input = CheckValidInput(0, 4);
+            switch (input)
+            {
+                case 0:
+                    //나가기
+                    break;
+                case 1:
+                    //1. 아이템 장착 -> 무기, 방어구, 물약을 따로 테이블 생성
+                    InventoryItemEquip();
+                    break;
+                case 2:
+                    //아이템 정렬 -> 무기, 방어구, 물약을 따로 테이블 생성
+                    InventorySort();
+                    break;
+                case 3:
+                    //아이템 정렬
+                    InventorySort();
+                    break;
+                case 4:
+                    //아이템 정렬
+                    InventorySort();
+                    break;
+            }
+        }
+
+        public void InventoryInfo()
+        {
+            DisplayInventory();
+
+            int input = CheckValidInput(0, 4);
+            switch (input)
+            {
+                case 0:
+                    //나가기
+                    break;
+                case 1:
+                    //1. 인벤토리 아이템 장착
+                    InventoryItemEquip();
+                    break;
+                case 2:
+                    //2. 인벤토리 정렬
+                    InventorySort();
+                    break;
+            }
+        }
+
+        //인벤토리 아이템 창작
+        public void InventoryItemEquip()
+        {
+            DisplayInventory();
+
+            int input = CheckValidInput(0, inventory.Count);
+            if (input == 0)
+            {
+                //나가기
+            }
+            else
+            {
+
+            }
+        }
+
+
+        //인벤토리 정렬
+        public void InventorySort()
+        {
+            DisplayInventory();
+
+            int input = CheckValidInput(0, 4);
+            switch (input)
+            {
+                case 0:
+                    //나가기
+                    break;
+                case 1:
+                    //공격력 높은 순으로 정렬
+                    break;
+                case 2:
+                    //방어력 높은 순으로 정렬
+                    break;
+                case 3:
+                    //포션을 상단으로 정렬
+                    break;
+            }
+        }
+
+        //입력값 확인
+        public static int CheckValidInput(int min, int max)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                bool parseSuccess = int.TryParse(input, out var ret);
+                if (parseSuccess)
+                {
+                    if (ret >= min && ret <= max)
+                        return ret;
+                }
+
+                Console.WriteLine("잘못된 입력입니다.");
+            }
+        }
 
     }
 }
+
+
+
--- PENTAGON/Item.cs
+++ PENTAGON/Item.cs
@@ -6,28 +6,106 @@ using System.Threading.Tasks;
 
 namespace PENTAGON
 {
+    public enum Job
+    {
+        Null,
+        Warrior,
+        Mage,
+        Archer,
+        Thief
+    }
+    public enum Type
+    {
+        Null,
+        Weapon,
+        Armor
+    }
+    //아이템 - 이름, 레벨
     public class Item
     {
+        public string Name { get; }
+        public int Level { get; }
+        public int Atk { get; }
+        public int Def { get; }
+        public string Explanation { get; }
+        public Job Job { get; }
+        public bool IsEquip { get; set; }
 
+        public Item(string name, int level, int atk, int def, string explanation, Job job, bool isEquip)
+        {
+            Name = name;
+            Level = level;
+            Atk = atk;
+            Def = def;
+            Explanation = explanation;
+            Job = job;
+            IsEquip = isEquip;
+        }
     }
 
     public class EquipItem : Item
     {
+        public EquipItem(string name, int level, int atk, int def, string explanation, Job job, bool isEquip)
+        : base(name, level, atk, def, explanation, job, isEquip)
+        {
+
+        }
+        //아이템이 작착이 되었는지?
+
+        //같은 종류의 아이템이면 교체
 
     }
 
     public class WeaponItem : EquipItem
     {
+        public WeaponItem(string name, int level, int atk, int def, string explanation, Job job, bool isEquip)
+        : base(name, level, atk, def, explanation, job, isEquip)
+        {
 
+        }
     }
 
     public class ArmorItem : EquipItem
     {
+        public ArmorItem(string name, int level, int atk, int def, string explanation, Job job, bool isEquip)
+        : base(name, level, atk, def, explanation, job, isEquip)
+        {
 
+        }
     }
 
-    public class PotionItem : Item
-    {
+    //public class PotionItem : Item
+    //{
+    //    public int Heel { get; }
+    //    public PotionItem(string name, int level, int atk, int def, string explanation, Job job, bool isEquip, int heel )
+    //    : base(name, level, atk, def, explanation, job, isEquip)
+    //    {
+    //        Heel = heel;
+    //    }
 
+    //    // 물약 먹기
+    //    public void EatPosion()
+    //    {
+
+    //    }
+    //}
+    public class PotionItem
+    {
+        public string Name { get; }
+        public int Heel { get; }
+        public string Explanation { get; }
+        public PotionItem(string name, int heel, string explanation)
+        {
+            Name = name;
+            Heel = heel;
+            Explanation = explanation;
+        }
+
+        // 물약 먹기
+        public void EatPosion()
+        {
+            //포션을 먹으면 플레이어.Hp += Posion.Heel
+        }
     }
 }
+
