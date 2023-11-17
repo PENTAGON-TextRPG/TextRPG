f5f5b5e81d5962da72174c2ba2e7a3bda3141dc0
정채운 <toyboy01113@gmail.com>
2023-11-17 18:02:18 +0900

    Monster class 구조 변경 MonsterManager class 생성

---
+++ PENTAGON/MonsterManager.cs
@@ -0,0 +1,19 @@
+﻿using EnumsNamespace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PENTAGON
+{
+    public class MonsterManager
+    {
+        public List<Monster> GetMonsterOfStage(StageType stageType)
+        {
+            Program program = new Program();
+
+            return program.MonsterDict[stageType];
+        }
+    }
+}
