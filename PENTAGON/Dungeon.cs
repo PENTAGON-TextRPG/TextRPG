08d49912e2c421675e09f472e21fc65c22e85286
GAMGDOG <chltjdwo98@gmail.com>
2023-11-18 20:53:33 +0900

    Update Dungeon.cs

--- PENTAGON/Dungeon.cs
+++ PENTAGON/Dungeon.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleTables;
 
 namespace PENTAGON
 {
     public class Dungeon
     {
-        Program program = new Program();
+        MonsterManager monsterManager = new MonsterManager();
+        StageType stage;
         public void DisplayDungeonIntro(Player player)
         {
             Console.Clear();
@@ -18,14 +20,14 @@ namespace PENTAGON
             Console.ResetColor();
             Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.\n이제 전투를 시작할 수 있습니다.\n");
             Console.WriteLine("1. 상태 보기");
-            Console.WriteLine($"2. 전투 시작");//player 클래스에서 해당 플레이어가 도달한 스테이지 변수 추가
+            Console.WriteLine("2. 스테이지 선택");
             Console.WriteLine("0. 나가기");
 
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요.");
             Console.Write(">>");
 
-            int input = program.CheckValidInput(0, 2);
+            int input = Program.CheckValidInput(0, 2);
             switch (input)
             {
                 case 1:
@@ -35,38 +37,71 @@ namespace PENTAGON
                     DisplayStage();//스테이지 선택 메서드
                     break;
                 case 0:
-                    program.DisplayGameIntro();
+                    Program.DisplayGameIntro();
                     break;
             }
         }//던전 입장 화면
 
         public void DisplayStage()
         {
+            var table = new ConsoleTable("층","추천 공격력","추천 방어력");
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("던전입장");
             Console.ResetColor();
-            Battle(Program.player1, List<Monster> MonsterLists);//현재 플레이어 도달 스테이지로 이동
+            Console.WriteLine();
+            Console.WriteLine("입장할 층을 입력해주세요.\n");
+
+            table.AddRow(1, "5", "3");
+            table.AddRow(2, "10", "5");
+            table.AddRow(3, "15", "10");
+            table.AddRow(4, "20","15");
+            table.AddRow(5, "25", "20");
+
+            Console.WriteLine(table);   //층 별 난이도 표시
+
+            int input = Program.CheckValidInput(1, 5);
+            switch(input)
+            {
+                case 1:
+                    stage = StageType.ST_One;
+                    break;
+                case 2:
+                    stage = StageType.ST_Two;
+                    break;
+                case 3:
+                    stage = StageType.ST_Three;
+                    break;
+                case 4:
+                    stage = StageType.ST_Four;
+                    break;
+                case 5:
+                    stage = StageType.ST_Five;
+                    break;
+            }
+
+            Battle(Program.player1, stage);//현재 플레이어 도달 스테이지로 이동
         }
-        public void Battle(Player player, List<Monster> monsters, StageType stageType)
+        public void Battle(Player player, StageType stageType)
         {
+            List<Monster> monsters = new List<Monster>(); //몬스터 리스트 선언
+            monsters = monsterManager.GetMonsterOfStage(stageType); //해당 스테이지 몬스터의 리스트 저장
             var random = new Random();
-            int monstercount = random.Next(1, 5); //출현하는 몬스터의 수 랜덤변수
-            var takemonster = monsters.OrderBy(x => x.Name).Take(monstercount); //출현하는 몬스터 수만큼 몬스터를 가져옴
+            int monstercount = random.Next(1,5); //출현할 몬스터의 수
+            List<Monster> stageMonster=monsters.OrderBy(x=>random.Next()).Take(monstercount).ToList(); //스테이지에 출현하는 몬스터 리스트 생성
             while (true)
             {
                 int IsWin = monstercount; //전투 승리 판정
                 Console.Clear();
                 Console.WriteLine("Battle!!");
-                int i = 0;
-                foreach(List<Monster> monster in takemonster)
+                for(int i =0;i<monstercount;i++)
                 {
-                    var monsterAlive = (monster[i].Hp > 0) ? (monster[i].Hp).ToString() : "Dead"; //몬스터의 체력이 0이하일때 Dead를 출력, 0보다 크면 그 숫자를 출력
-                    Console.WriteLine($"{monster[i].Name} HP {monsterAlive}");
+                    var monsterAlive = (stageMonster[i].Hp > 0) ? (stageMonster[i].Hp).ToString() : "Dead"; //몬스터의 체력이 0이하일때 Dead를 출력, 0보다 크면 그 숫자를 출력
+                    Console.WriteLine($"{i+1}. {stageMonster[i].Name} HP {monsterAlive}");
                     if(monsterAlive == "Dead")
                     {
                         IsWin--;//몬스터가 죽었으면 판정
-                        monster[].IsDie();
+                        stageMonster[i].IsDie();
                         if(IsWin == 0) //몬스터가 모두 죽었을때 전투승리 판정
                         {
                             Console.WriteLine("모든 몬스터를 처치했습니다.");
@@ -88,15 +123,15 @@ namespace PENTAGON
                 Console.WriteLine();
                 Console.WriteLine("원하시는 행동을 입력해주세요.");
                 Console.Write(">>");
-                int input = program.CheckValidInput(1, 3);
+                int input = Program.CheckValidInput(1, 3);
 
                 switch (input)
                 {
                     case 1:
                         Console.WriteLine("공격할 몬스터를 선택해 주세요.");
                         Console.Write(">>");
-                        int select = program.CheckValidInput(1, monstercount);
-                        player.Attack(monsters[select - 1]);//플레이어 공격 처리(몬스터 데미지 계산)
+                        int select = Program.CheckValidInput(1, monstercount);
+                        player.Attack(stageMonster[select - 1]);//플레이어 공격 처리(몬스터 데미지 계산)
                         //데미지 계산 처리에서 몬스터가 죽으면 player 경험치 획득 메서드 실행
                         break;
                     case 2:
