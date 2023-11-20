8f94ddd2600e24f8943a73cd11603868c5439e5f
yskybright <rgh99@naver.com>
2023-11-20 15:24:50 +0900

    StoreBuy 구현 완료 (오버로드 필요)
    Inventory 클래스와 오버로드가 필요합니다.

--- PENTAGON/Store.cs
+++ PENTAGON/Store.cs
@@ -14,6 +14,7 @@ namespace PENTAGON
         private static List<Item> StoreWeapon = new List<Item>(); // 무기 
         private static List<Item> StoreArmor = new List<Item>(); // 방어구
         private static List<Item> StorePotion = new List<Item>(); // 포션 
+        Inventory WeaponInventory = new Inventory();
 
         static int CheckValidInput(int min, int max)
         {
@@ -42,38 +43,38 @@ namespace PENTAGON
         static void StoreSetting()
         {
             // 무기 목록 (능력치와 골드는 임시값)
-            ArmorItem w3 = new ArmorItem("전사3(W)", 3, JobType.JT_Warrior, 3, 0, 0, "공격력 +3", "3레벨 전사 무기.", 300, false);
-            EquipItem bloodyspear = new EquipItem("핏빛 창(W)", 3, JobType.JT_Warrior, 6, 0, 0, "공격력 +6", "전장의 핏물이 이룬 살기로 가득합니다.", 600, false);
-            EquipItem w9 = new EquipItem("전사9(W)", 3, JobType.JT_Warrior, 9, 0, 0, "공격력 +9", "9레벨 전사 무기.", 900, false);
+            WeaponItem w3 = new WeaponItem("전사3(W)", 3, JobType.JT_Warrior, 3, "공격력 +3", "3레벨 전사 무기.", 300, false);
+            WeaponItem bloodyspear = new WeaponItem("핏빛 창(W)", 3, JobType.JT_Warrior, 6, "공격력 +6", "전장의 핏물이 이룬 살기로 가득합니다.", 600, false);
+            WeaponItem w9 = new WeaponItem("전사9(W)", 3, JobType.JT_Warrior, 9, "공격력 +9", "9레벨 전사 무기.", 900, false);
 
-            EquipItem m3 = new EquipItem("마법사3(W)", 3, JobType.JT_Mage, 3, 0, 0, "공격력 +3", "3레벨 마법사 무기.", 300, false);
-            EquipItem pinkvenom = new EquipItem("핑크 베놈(W)", 3, JobType.JT_Mage, 6, 0, 0, "공격력 +6", "진분홍 살모사의 독이 서린 스태프입니다.", 600, false);
-            EquipItem m9 = new EquipItem("마법사9(W)", 3, JobType.JT_Mage, 9, 0, 0, "공격력 +9", "9레벨 마법사 무기.", 900, false);
+            WeaponItem m3 = new WeaponItem("마법사3(W)", 3, JobType.JT_Mage, 3, "공격력 +3", "3레벨 마법사 무기.", 300, false);
+            WeaponItem pinkvenom = new WeaponItem("핑크 베놈(W)", 3, JobType.JT_Mage, 6, "공격력 +6", "진분홍 살모사의 독이 서린 스태프입니다.", 600, false);
+            WeaponItem m9 = new WeaponItem("마법사9(W)", 3, JobType.JT_Mage, 9, "공격력 +9", "9레벨 마법사 무기.", 900, false);
 
-            EquipItem t3 = new EquipItem("도적3(W)", 3, JobType.JT_Thief, 3, 0, 0, "공격력 +3", "3레벨 도적 무기.", 300, false);
-            EquipItem t6 = new EquipItem("도적6(W)", 3, JobType.JT_Thief, 6, 0, 0, "공격력 +6", "6레벨 도적 무기.", 600, false);
-            EquipItem lunarblade = new EquipItem("월식(W)", 3, JobType.JT_Thief, 9, 0, 0, "공격력 +9", "황혼이 머문 자리에 깃든 만월의 축복.", 900, false);
+            WeaponItem t3 = new WeaponItem("도적3(W)", 3, JobType.JT_Thief, 3, "공격력 +3", "3레벨 도적 무기.", 300, false);
+            WeaponItem t6 = new WeaponItem("도적6(W)", 3, JobType.JT_Thief, 6, "공격력 +6", "6레벨 도적 무기.", 600, false);
+            WeaponItem lunarblade = new WeaponItem("월식(W)", 3, JobType.JT_Thief, 9, "공격력 +9", "황혼이 머문 자리에 깃든 만월의 축복.", 900, false);
 
-            EquipItem a3 = new EquipItem("궁수3(W)", 3, JobType.JT_Archer, 3, 0, 0, "공격력 +3", "3레벨 궁수 무기.", 300, false);
-            EquipItem a6 = new EquipItem("궁수6(W)", 3, JobType.JT_Archer, 6, 0, 0, "공격력 +6", "6레벨 궁수 무기.", 600, false);
-            EquipItem a9 = new EquipItem("궁수9(W)", 3, JobType.JT_Archer, 9, 0, 0, "공격력 +9", "9레벨 궁수 무기.", 900, false);
+            WeaponItem a3 = new WeaponItem("궁수3(W)", 3, JobType.JT_Archer, 3, "공격력 +3", "3레벨 궁수 무기.", 300, false);
+            WeaponItem a6 = new WeaponItem("궁수6(W)", 3, JobType.JT_Archer, 6, "공격력 +6", "6레벨 궁수 무기.", 600, false);
+            WeaponItem a9 = new WeaponItem("궁수9(W)", 3, JobType.JT_Archer, 9, "공격력 +9", "9레벨 궁수 무기.", 900, false);
 
             // 방어구 목록 (능력치와 골드는 임시값)
-            EquipItem thornmail = new EquipItem("가시갑옷(A)", 5, JobType.JT_Warrior, 0, 5, 0, "방어력 +5", "날카로운 가시들의 부드러운 춤.", 500, false);
-            EquipItem goldenplate = new EquipItem("황금갑옷(A)", 10, JobType.JT_Warrior, 0, 10, 0, "방어력 +10", "번뜩이는 흉갑에 적의 눈동자가 스칩니다.", 1000, false);
+            ArmorItem thornmail = new ArmorItem("가시갑옷(A)", 5, JobType.JT_Warrior, 5, 10, "방어력 +5, 체력 +10", "날카로운 가시들의 부드러운 춤.", 500, false);
+            ArmorItem goldenplate = new ArmorItem("황금갑옷(A)", 10, JobType.JT_Warrior, 10, 20, "방어력 +10, 체력 +20", "번뜩이는 흉갑에 적의 눈동자가 스칩니다.", 1000, false);
 
-            EquipItem m5 = new EquipItem("마법사5(A)", 5, JobType.JT_Mage, 0, 5, 0, "방어력 +5", "5레벨 마법사 방어구.", 500, false);
-            EquipItem m10 = new EquipItem("마법사10(A)", 10, JobType.JT_Mage, 0, 10, 0, "방어력 +10", "10레벨 마법사 방어구.", 1000, false);
+            ArmorItem m5 = new ArmorItem("마법사5(A)", 5, JobType.JT_Mage, 5, 10, "방어력 +5, 체력 +10", "5레벨 마법사 방어구.", 500, false);
+            ArmorItem m10 = new ArmorItem("마법사10(A)", 10, JobType.JT_Mage, 10, 20, "방어력 +10, 체력 +20", "10레벨 마법사 방어구.", 1000, false);
 
-            EquipItem t5 = new EquipItem("도적5(A)", 5, JobType.JT_Thief, 0, 5, 0, "방어력 +5", "5레벨 도적 방어구.", 500, false);
-            EquipItem t10 = new EquipItem("도적10(A)", 10, JobType.JT_Thief, 0, 10, 0, "방어력 +10", "10레벨 도적 방어구.", 1000, false);
+            ArmorItem t5 = new ArmorItem("도적5(A)", 5, JobType.JT_Thief, 5, 10, "방어력 +5, 체력 +10", "5레벨 도적 방어구.", 500, false);
+            ArmorItem t10 = new ArmorItem("도적10(A)", 10, JobType.JT_Thief, 10, 20, "방어력 +10, 체력 +20", "10레벨 도적 방어구.", 1000, false);
 
-            EquipItem a5 = new EquipItem("궁수5(A)", 5, JobType.JT_Archer, 0, 5, 0, "방어력 +5", "5레벨 궁수 방어구.", 500, false);
-            EquipItem thunderdash = new EquipItem("번개질주(A)", 10, JobType.JT_Archer, 0, 10, 0, "방어력 +10", "누구보다 빛나고 싶은 자들의 우상.", 1000, false);
+            ArmorItem a5 = new ArmorItem("궁수5(A)", 5, JobType.JT_Archer, 5, 10, "방어력 +5, 체력 +10", "5레벨 궁수 방어구.", 500, false);
+            ArmorItem thunderdash = new ArmorItem("번개질주(A)", 10, JobType.JT_Archer, 10, 20, "방어력 +10, 체력 +20", "누구보다 빛나고 싶은 자들의 우상.", 1000, false);
 
             // 포션 목록 (능력치와 골드는 임시값)
-            PotionItem redpotion = new PotionItem("빨간 물약", 1, 0, 0, 10, "HP +10.", "사용 시 HP를 10 회복합니다.", 30, false);
-            PotionItem bluepotion = new PotionItem("파란 물약", 5, 0, 0, 10, "HP +30.", "사용 시 MP를 10 회복합니다.", 30, false);
+            PotionItem redpotion = new PotionItem("빨간 물약", 30, 0, 0, "HP +10.", "사용 시 HP를 30 회복합니다.", 30);
+            PotionItem bluepotion = new PotionItem("파란 물약", 0, 30, 0, "MP +30.", "사용 시 MP를 30 회복합니다.", 30);
 
 
 
@@ -96,9 +97,6 @@ namespace PENTAGON
             StoreWeapon.Add(a9);
 
             // 방어구 Add 
-            // Null
-            StoreArmor.Add(null3);
-            StoreArmor.Add(null7);
             // Warrior
             StoreArmor.Add(thornmail);
             StoreArmor.Add(goldenplate);
@@ -114,7 +112,7 @@ namespace PENTAGON
 
             // 포션 Add
             StorePotion.Add(redpotion);
-            StorePotion.Add(whitepotion);
+            StorePotion.Add(bluepotion);
 
 
         }
@@ -152,7 +150,7 @@ namespace PENTAGON
             Console.Clear();
             ShowHighlightedText("상점 - 구매");
             Console.WriteLine("[상점 주인 아만다] : 우리 물건이 제일 좋다구!");
-            Console.WriteLine("[" + player.Name + "의 Gold]" + " : " + player.Gold + " G\n"); // 플레이어 메서드 가져오기
+            Console.WriteLine("[" + Program.player1.Name + "의 Gold]" + " : " + Program.player1.Gold + " G\n");
             Console.WriteLine();
 
             int input = CheckValidInput(0, 3);
@@ -162,69 +160,72 @@ namespace PENTAGON
                     GameManager.Instance.DisplayGameIntro();
                     break;
                 case 1:
-                    DisplayStoreWeapon();
+                    StoreBuyWeapon();
                     break;
                 case 2:
-                    DisplayStoreArmor();
+                    StoreBuyArmor();
                     break;
                 case 3:
-                    DisplayStorePotion();
+                    StoreBuyPotion();
                     break;
             }
 
             Console.WriteLine("0. 나가기");
-            Console.WriteLine("1. 무기 상점");
-            Console.WriteLine("2. 방어구 상점");
-            Console.WriteLine("3. 물약 상점");
+            Console.WriteLine("1. 무기 구매");
+            Console.WriteLine("2. 방어구 구매");
+            Console.WriteLine("3. 물약 구매");
         }
 
-        static void DisplayStoreWeapon()
+        static void StoreBuyWeapon()
         {
             Console.Clear();
-            ShowHighlightedText("무기 상점");
+            ShowHighlightedText("무기 구매");
             Console.WriteLine("[상점 주인 아만다] : 말보다 무기로 기선제압! 우리 스타일 알지? ");
-            Console.WriteLine("[" + player.Name + "의 Gold]" + " : " + player.Gold + " G\n"); // 플레이어 메서드 가져오기
+            Console.WriteLine("[" + Program.player1.Name + "의 Gold]" + " : " + Program.player1.Gold + " G\n");
             Console.WriteLine();
 
             Console.WriteLine("\n[아이템 목록]");
-            ConsoleTable table = new ConsoleTable("아이템 이름", "레벨", "직업", "효과", "설명", "Gold");
+            ConsoleTable table = new ConsoleTable("무기", "레벨", "직업", "효과", "설명", "Gold");
             for (int i = 0; i < StoreWeapon.Count; i++)
             {
-                if (Inventory.player(StoreWeapon[i]))  // 인벤토리에 아이템이 있는지 없는지 확인할 메서드가 필요
+                if (Inventory.WeaponInventory(StoreWeapon[i]))  // 인벤토리에 아이템이 있는지 확인 
                 {
-                    table.AddRow(StoreWeapon[i].Name, StoreWeapon[i].Level , StoreWeapon[i].Job , StoreWeapon[i].Effect, StoreWeapon[i].Explanation, "구매 완료");
+                    table.AddRow(StoreWeapon[i].Name, StoreWeapon[i].Level, StoreWeapon[i].JobType, StoreWeapon[i].Effect, StoreWeapon[i].Explanation, "구매 완료");
                 }
                 else
                 {
-                    table.AddRow(i + 1 + ". " + StoreWeapon[i].Name, StoreWeapon[i].Level, StoreWeapon[i].Job, StoreWeapon[i].Effect, StoreWeapon[i].Explanation, StoreWeapon[i].Price);
+                    table.AddRow(i + 1 + ". " + StoreWeapon[i].Name, StoreWeapon[i].Level, StoreWeapon[i].JobType, StoreWeapon[i].Effect, StoreWeapon[i].Explanation, StoreWeapon[i].Gold);
                 }
             }
+
+            table.Write();
+            Console.WriteLine("0. 나가기");
 
             while (true)
             {
                 Console.WriteLine();
-                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.WriteLine("아이템을 선택해주세요.");
                 Console.Write(">>");
                 int input = CheckValidInput(0, StoreWeapon.Count);
                 if (input == 0)
                 {
-                    GameManager.Instance.DisplayGameIntro();
+                    StoreBuy();
                 }
                 else
                 {
-                    if (Inventory.xx(StoreWeapon[input - 1])) // 인벤토리에 아이템이 있는지 없는지 확인할 메서드가 필요
+                    if (Inventory.WeaponInventory(StoreWeapon[input - 1])) // 인벤토리에 아이템이 있는지 확인
                     {
                         Console.WriteLine("이미 구매한 아이템입니다.");
                     }
-                    else if (player.Gold >= StoreWeapon[input - 1].Gold)  // 인벤토리에 골드 메서드 필요
+                    else if (Program.player1.Gold >= StoreWeapon[input - 1].Gold)
                     {
-                        player.Gold -= StoreWeapon[input - 1].Gold;
-                        Inventory.Add(StoreWeapon[input - 1]);
+                        Program.player1.Gold -= StoreWeapon[input - 1].Gold;
+                        Inventory.WeaponInventory.Add(StoreWeapon[input - 1]);
                         Console.WriteLine("구매하는 중.. 잠시만 기다려주세요.");
                         Thread.Sleep(1000);
-                        StoreMain();
+                        StoreBuyWeapon();
                     }
-                    else if (player.Gold < StoreWeapon[input - 1].Gold)
+                    else if (Program.player1.Gold < StoreWeapon[input - 1].Gold)
                     {
                         Console.WriteLine("Gold가 부족합니다.");
                     }
@@ -232,40 +233,177 @@ namespace PENTAGON
             }
         }
 
-        static void DisplayStoreArmor()
+
+        static void StoreBuyArmor()
         {
             Console.Clear();
-            ShowHighlightedText("방어구 상점");
+            ShowHighlightedText("방어구 구매");
             Console.WriteLine("[상점 주인 아만다] : 잘 막고 버텨야 때릴 시간도 생기는 법이지. ");
-            Console.WriteLine("[" + player.Name + "의 Gold]" + " : " + player.Gold + " G\n"); // 플레이어 메서드 가져오기
+            Console.WriteLine("[" + Program.player1.Name + "의 Gold]" + " : " + Program.player1.Gold + " G\n");
             Console.WriteLine();
+
+            Console.WriteLine("\n[아이템 목록]");
+            ConsoleTable table = new ConsoleTable("방어구", "레벨", "직업", "효과", "설명", "Gold");
+            for (int i = 0; i < StoreArmor.Count; i++)
+            {
+                if (Inventory.ArmorInventory(StoreArmor[i]))  // 인벤토리에 아이템이 있는지 확인 
+                {
+                    table.AddRow(StoreArmor[i].Name, StoreArmor[i].Level, StoreArmor[i].JobType, StoreArmor[i].Effect, StoreArmor[i].Explanation, "구매 완료");
+                }
+                else
+                {
+                    table.AddRow(i + 1 + ". " + StoreArmor[i].Name, StoreArmor[i].Level, StoreArmor[i].JobType, StoreArmor[i].Effect, StoreArmor[i].Explanation, StoreArmor[i].Gold);
+                }
+            }
+
+            table.Write();
+            Console.WriteLine("0. 나가기");
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("아이템을 선택해주세요.");
+                Console.Write(">>");
+                int input = CheckValidInput(0, StoreArmor.Count);
+                if (input == 0)
+                {
+                    StoreBuy();
+                }
+                else
+                {
+                    if (Inventory.AromorInventory(StoreArmor[input - 1])) // 인벤토리에 아이템이 있는지 확인
+                    {
+                        Console.WriteLine("이미 구매한 아이템입니다.");
+                    }
+                    else if (Program.player1.Gold >= StoreArmor[input - 1].Gold)
+                    {
+                        Program.player1.Gold -= StoreArmor[input - 1].Gold;
+                        Inventory.ArmorInventory.Add(StoreArmor[input - 1]);
+                        Console.WriteLine("구매하는 중.. 잠시만 기다려주세요.");
+                        Thread.Sleep(1000);
+                        StoreBuyArmor();
+                    }
+                    else if (Program.player1.Gold < StoreArmor[input - 1].Gold)
+                    {
+                        Console.WriteLine("Gold가 부족합니다.");
+                    }
+                }
+            }
         }
 
-        static void DisplayStorePotion()
+        static void StoreBuyPotion()
         {
             Console.Clear();
-            ShowHighlightedText("물약 상점");
+            ShowHighlightedText("물약 구매");
             Console.WriteLine("[상점 주인 아만다] : 방금 만든 따끈따끈한 물약 어때?");
-            Console.WriteLine("[" + player.Name + "의 Gold]" + " : " + player.Gold + " G\n"); // 플레이어 메서드 가져오기
+            Console.WriteLine("[" + Program.player1.Name + "의 Gold]" + " : " + Program.player1.Gold + " G\n");
             Console.WriteLine();
+
+            Console.WriteLine("\n[아이템 목록]");
+            ConsoleTable table = new ConsoleTable("물약", "설명", "Gold");
+            for (int i = 0; i < StorePotion.Count; i++)
+            {
+                table.AddRow(i + 1 + ". " + StorePotion[i].Name, StorePotion[i].Explanation, StorePotion[i].Gold);
+            }
+
+            table.Write();
+            Console.WriteLine("0. 나가기");
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("아이템을 선택해주세요.");
+                Console.Write(">>");
+                int input = CheckValidInput(0, StorePotion.Count);
+                if (input == 0)
+                {
+                    StoreBuy();
+                }
+                else
+                {
+                    if (Program.player1.Gold >= StoreWeapon[input - 1].Gold)
+                    {
+                        Program.player1.Gold -= StoreArmor[input - 1].Gold;
+                        Inventory.ETCInventory.Add(StoreArmor[input - 1]);
+                        Console.WriteLine("구매하는 중.. 잠시만 기다려주세요.");
+                        Thread.Sleep(1000);
+                        StoreBuyPotion();
+                    }
+                    else if (Program.player1.Gold < StoreArmor[input - 1].Gold)
+                    {
+                        Console.WriteLine("Gold가 부족합니다.");
+                    }
+                }
+            }
+
+
         }
 
         static void StoreSell()
         {
             Console.Clear();
             ShowHighlightedText("상점 - 판매");
-            Console.WriteLine("[상점 주인 아만다] : 어디 보자.. 쓸만 한 게 있을까?");
-            Console.WriteLine("[" + PlayerManager.nickname + "의 Gold]" + " : " + player.Gold + " G\n"); // 플레이어 메서드 가져오기
+            Console.WriteLine("[상점 주인 아만다] : 흠.. 쓸만한 게 있나 볼까?");
+            Console.WriteLine("[" + Program.player1.Name + "의 Gold]" + " : " + Program.player1.Gold + " G\n");
             Console.WriteLine();
-            Console.WriteLine("\n[아이템 목록]");
-            ConsoleTable table = new ConsoleTable("아이템 이름", "레벨", "효과", "설명", "Gold");
 
+            int input = CheckValidInput(0, 3);
+            switch (input)
+            {
+                case 0:
+                    GameManager.Instance.DisplayGameIntro();
+                    break;
+                case 1:
+                    StoreSellWeapon();
+                    break;
+                case 2:
+                    StoreSellArmor();
+                    break;
+                case 3:
+                    StoreSellPotion();
+                    break;
+            }
 
-            table.Write();
             Console.WriteLine("0. 나가기");
             Console.WriteLine("1. 무기 판매");
             Console.WriteLine("2. 방어구 판매");
             Console.WriteLine("3. 물약 판매");
+        }
+
+        static void StoreSellWeapon()
+        {
+            Console.Clear();
+            ShowHighlightedText("무기 판매");
+            Console.WriteLine("[상점 주인 아만다] : 흠.. 쓸만한 게 있나 볼까?");
+            Console.WriteLine("[" + Program.player1.Name + "의 Gold]" + " : " + Program.player1.Gold + " G\n");
+            Console.WriteLine();
+            Console.WriteLine("\n[아이템 목록]");
+            ConsoleTable table = new ConsoleTable("아이템 이름", "레벨", "직업", "효과", "설명", "Gold");
+
+        }
+
+        static void StoreSellArmor()
+        {
+            Console.Clear();
+            ShowHighlightedText("방어구 판매");
+            Console.WriteLine("[상점 주인 아만다] : 흠.. 쓸만한 게 있나 볼까?");
+            Console.WriteLine("[" + Program.player1.Name + "의 Gold]" + " : " + Program.player1.Gold + " G\n");
+            Console.WriteLine();
+            Console.WriteLine("\n[아이템 목록]");
+            ConsoleTable table = new ConsoleTable("아이템 이름", "레벨", "직업", "효과", "설명", "Gold");
+
+        }
+
+        static void StoreSellPotion()
+        {
+            Console.Clear();
+            ShowHighlightedText("물약 판매");
+            Console.WriteLine("[상점 주인 아만다] : 흠.. 쓸만한 게 있나 볼까?");
+            Console.WriteLine("[" + Program.player1.Name + "의 Gold]" + " : " + Program.player1.Gold + " G\n");
+            Console.WriteLine();
+            Console.WriteLine("\n[아이템 목록]");
+            ConsoleTable table = new ConsoleTable("아이템 이름", "레벨", "직업", "효과", "설명", "Gold");
+
         }
     }
 }
